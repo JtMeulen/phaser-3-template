ee79c9e40499d47ef7b335a28d310894cf456f7d
Joey ter Meulen <joeytermeulen@gmail.com>
2019-06-17 21:28:49 +0200

    add assets

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,22 +1,26 @@
 import { Scene } from 'phaser';
-import logoImg from "../assets/logo.png";
+
+import bombImg from "../assets/bomb.png";
+import starImg from "../assets/star.png";
+import plaformImg from "../assets/platform.png";
+import skyImg from "../assets/sky.png";
+import dudeSprite from "../assets/dude.png";
 
 class GameScene extends Scene {
   preload() {
-    this.load.image("logo", logoImg);
+    this.load.image("sky", skyImg);
+    this.load.image("platform", plaformImg);
+    this.load.image("star", starImg);
+    this.load.image("bomb", bombImg);
+    this.load.spritesheet("dude", dudeSprite, {
+      frameWidth: 32, frameHeight: 48
+    });
   }
 
   create() {
-    const logo = this.add.image(400, 150, "logo");
-
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1
-    });
+    const sky = this.add.image(0, 0, "sky").setOrigin(0,0);
+    const platform = this.add.image(0, 350, "platform").setOrigin(0,0);
+    const star = this.add.image(400, 300, "star");
   }
 }
 
