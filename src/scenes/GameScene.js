e741113ab490bac6c0c0b761411a2be9bebf2828
Joey ter Meulen <joeytermeulen@gmail.com>
2019-06-18 21:08:20 +0200

    add player sprite

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -22,6 +22,7 @@ class GameScene extends Scene {
     const star = this.add.image(400, 300, "star");
 
     this.createPlatforms();
+    this.createPlayer();
   }
 
   createPlatforms() {
@@ -30,6 +31,38 @@ class GameScene extends Scene {
     this.platforms.create(150, 360, 'ground');
     this.platforms.create(700, 220, 'ground');
     this.platforms.create(180, 70, 'ground');
+  }
+
+  createPlayer() {
+    this.player = this.physics.add.sprite(400, 100, 'dude');
+    this.player.setBounce(0.2);
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 0, end: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{key: 'dude', frame: 4}],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 5, end: 8
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
   }
 }
 
